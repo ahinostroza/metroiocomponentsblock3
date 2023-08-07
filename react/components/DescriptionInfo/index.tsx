cbebd9893c628b1f853f6f8421026068cee9f52c
Juan Siesquen <juan.siesquenrosales@cencosud.com.pe>
2023-08-07 15:54:06 -0500

    fix(flixmedia): :bug: 20664 Integración sobre Flixmedia
    Updated with a better capture of product model from product name

--- react/components/DescriptionInfo/index.tsx
+++ react/components/DescriptionInfo/index.tsx
@@ -1,58 +1,64 @@
 import React, { useEffect, useState } from 'react'
-import { Helmet } from "react-helmet";
-import { useProduct } from 'vtex.product-context';
+import { useProduct } from 'vtex.product-context'
+import { Helmet } from "react-helmet"
 
 const DescriptionInfo = () => {
+  const modelpattern = /\ [A-Z|1-9][A-Z\-0-9]{5,}/
   const product = useProduct()
-  const [mpn, setMpn] = useState<any>(null)
-  const [brand, setBrand] = useState<any>(null)
-  const insertAfter = (newNode: any, existingNode: any) => {
-    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+  const [mpn, setMpn] = useState<any>("")
+  const [ean, setEan] = useState<any>("")
+  const [brand, setBrand] = useState<any>("")
+
+  const settingMedia = (product: any) => {
+    if (!window?.document || !product?.selectedItem?.name) return
+
+    const modelMatch = product.selectedItem.name.match(modelpattern)
+    let model = product?.product?.properties?.filter((prop: any) => prop.name === 'Modelo')[0]?.values?.filter((value: any) => value)[0]
+    model = model ?? (modelMatch ? modelMatch[0].trim() : "")
+
+    setMpn(model)
+    setEan(product?.product?.items[0]?.ean || "")
+    setBrand(product?.product?.brand?.toUpperCase() || "")
   }
 
   useEffect(() => {
-    const container = window?.document?.createElement("div");
-    container.id = "flix-minisite";
-    setMpn(product?.selectedItem?.name.split('\"')[1] != undefined ? product?.selectedItem?.name.split('\"')[1].trim().toUpperCase() : null)
-    setBrand(product?.product?.brand.toUpperCase())
-    setTimeout(() => {
-      insertAfter(container, window?.document?.getElementsByClassName('vtex-tab-layout-0-x-contentContainer')[0]);
-    }, 1500)
-  }, [])
+    settingMedia(product)
+  }, [product])
 
   return (
     <>
       {
-        mpn != null && brand != null ? (
+        brand !== '' ? (
           <Helmet>
+            <style>
+              {
+                `
+                #flix-inpage { width: 80%; }
+                @media only screen and (max-width:768px) {
+                  #flix-std-inpage.flix-samsung-t3 .flix-std-content {
+                    position:absolute !important;
+                  }
+                }
+                `
+              }
+            </style>
             <script type="text/javascript"
               data-flix-distributor="12814"
               data-flix-language="pe"
               data-flix-fallback-language="t2"
               data-flix-brand={brand}
-              data-flix-ean=""
+              data-flix-ean={ean}
               data-flix-mpn={mpn}
               data-flix-inpage="flix-inpage"
               data-flix-button="flix-minisite"
               data-flix-price=""
               src="//media.flixfacts.com/js/loader.js">
             </script>
-
-            <style>
-              {
-                `
-                  @media only screen and (max-width: 768px){
-                    #flix-std-inpage.flix-samsung-t3 .flix-std-content{
-                      position:absolute !important;
-                    }
-                  }
-                  `
-              }
-            </style>
           </Helmet>
-        ) : ''
+        ) : <></>
       }
     </>
   )
 }
+
 export default DescriptionInfo
