120abedc182ac366fdb0cb35a642aa1b7c208c07
Angel Hinostroza <angeljose.hinostrozacalderon@externos-pe.cencosud.com>
2023-10-03 15:45:31 -0500

    21860 - ajustar comentario

--- react/AccountPage.tsx
+++ react/AccountPage.tsx
@@ -5,11 +5,11 @@ import { Route } from 'vtex.my-account-commons/Router'
 // Your component pages
 import OrderListPage from './OrderListPage'
 
-const AccountPageWishlist = () => (
+const AccountPage = () => (
   <Fragment>
     {/* This `path` will be added at the end of the URL */}
     <Route path="/orders-legacy" exact component={OrderListPage} />
   </Fragment>
 )
 
-export default AccountPageWishlist
+export default AccountPage
